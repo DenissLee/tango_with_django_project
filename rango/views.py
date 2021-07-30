bc4a78cbd327c2876e652d05a97afda730146834
YOU LI <2600058L@student.gla.ac.uk>
2021-07-30 03:08:57 +0100

    view about

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    l="rango says hey there partner!"
+    l="Rango say hey there partner! <a href='/rango/about/'>About</a >"
     return HttpResponse(l)
  
+def about(request):
+    l1="Rango sayas here is another method <a href='/rango/'>Index</a >"
+    return HttpResponse(l1)
