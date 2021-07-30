ed05a58e6afeba009fa298587206f84d7f4dfaab
YOU LI <2600058L@student.gla.ac.uk>
2021-07-30 01:24:49 +0100

    creating a view

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    l="rango says hey there partner!"
+    return HttpResponse(l)
+ 
