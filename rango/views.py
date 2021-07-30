81337adc49e99147ee09886850463b7b1b30b342
YOU LI <2600058L@student.gla.ac.uk>
2021-07-30 03:21:00 +0100

    exercise for chapter3

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    l="Rango say hey there partner! <a href='/rango/about/'>About</a >"
-    return HttpResponse(l)
- 
-def about(request):
-    l1="Rango sayas here is another method <a href='/rango/'>Index</a >"
+    l1="Rango say hey there partner! <a href='/rango/about/'>About</a >"
     return HttpResponse(l1)
+
+def about(request):
+    l2="Rango sayas here is another method <a href='/rango/'>Index</a >"
+    return HttpResponse(l2)
